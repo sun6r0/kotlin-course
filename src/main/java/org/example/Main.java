6e26b58b4dab6d61398baaafe36765ae9f71ded2
sun6r0 <dshvaiko7@gmail.com>
2025-09-08 10:25:01 +0300

    Commit for a new brahcn

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;
 
-// Тестовый комментарий
+// Коммент для новой ветки
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
